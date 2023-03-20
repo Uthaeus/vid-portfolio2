1adc53a6ba4dbfb090d3952fffdff9326e940f22
Uthaeus <romanlavery@gmail.com>
2023-03-20 13:53:54 -0600

    created projects component and created dummy_data for dev use

---
+++ src/store/dummy_data.js
@@ -0,0 +1,57 @@
+
+export const DUMMY_DATA = [
+    {
+        id: 'p1',
+        data: {
+            title: 'title one',
+            description: 'this is a description of an item and it needs to be a little longer so here it is whooop',
+            thumbnail_url: 'http://some-image-url.org',
+            video_url: 'http://some-video-url.net'
+        }
+    },
+    {
+        id: 'p2',
+        data: {
+            title: 'title two',
+            description: 'this is a description of an item and it needs to be a little longer so here it is whooop',
+            thumbnail_url: 'http://some-image-url.org',
+            video_url: 'http://some-video-url.net'
+        }
+    },
+    {
+        id: 'p3',
+        data: {
+            title: 'title three',
+            description: 'this is a description of an item and it needs to be a little longer so here it is whooop',
+            thumbnail_url: 'http://some-image-url.org',
+            video_url: 'http://some-video-url.net'
+        }
+    },
+    {
+        id: 'p4',
+        data: {
+            title: 'title four',
+            description: 'this is a description of an item and it needs to be a little longer so here it is whooop',
+            thumbnail_url: 'http://some-image-url.org',
+            video_url: 'http://some-video-url.net'
+        }
+    },
+    {
+        id: 'p5',
+        data: {
+            title: 'title five',
+            description: 'this is a description of an item and it needs to be a little longer so here it is whooop',
+            thumbnail_url: 'http://some-image-url.org',
+            video_url: 'http://some-video-url.net'
+        }
+    },
+    {
+        id: 'p6',
+        data: {
+            title: 'title six',
+            description: 'this is a description of an item and it needs to be a little longer so here it is whooop',
+            thumbnail_url: 'http://some-image-url.org',
+            video_url: 'http://some-video-url.net'
+        }
+    },
+]
