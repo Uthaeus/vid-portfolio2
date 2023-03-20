7df4ecb997548f293b284cf3e5944de460e38978
Uthaeus <romanlavery@gmail.com>
2023-03-20 15:22:26 -0600

    refactored main navigation, adjusted styling in base and navigation

--- src/components/MainNavigation.js
+++ src/components/MainNavigation.js
@@ -1,24 +1,24 @@
 import { NavLink } from "react-router-dom";
 
 function MainNavigation() {
-    return (
-        <div className="main-nav-wrapper">
-            <div className="nav-title">Title</div>
-            <div className="nav-links-wrapper">
-                <ul>
-                    <li>
-                        <NavLink to='/' className='nav-link' end>Home</NavLink>
-                    </li>
-                    <li>
-                        <NavLink to='/about' className='nav-link'>About</NavLink>
-                    </li>
-                    <li>
-                        <NavLink to='/contact' className='nav-link'>Contact</NavLink>
-                    </li>
-                </ul>
-            </div>
-        </div>
-    );
+  return (
+    <div className="main-nav-wrapper">
+      <div className="nav-title">Homer J Simpson</div>
+      <div className="nav-links-wrapper">
+        <NavLink to="/" className="nav-link" end>
+          Home
+        </NavLink>
+
+        <NavLink to="/about" className="nav-link">
+          About
+        </NavLink>
+
+        <NavLink to="/contact" className="nav-link">
+          Contact
+        </NavLink>
+      </div>
+    </div>
+  );
 }
 
 export default MainNavigation;
