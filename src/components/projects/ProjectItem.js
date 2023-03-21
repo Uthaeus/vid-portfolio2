40a713aa4ea38a74ab9779c11238609c3c96ace0
Uthaeus <romanlavery@gmail.com>
2023-03-21 17:55:26 -0600

    moved project item image from image element to background

--- src/components/projects/ProjectItem.js
+++ src/components/projects/ProjectItem.js
@@ -1,11 +1,16 @@
 import { Link } from 'react-router-dom';
 
+import background from '../../assets/images/space.png';
+
 function ProjectItem({id, data}) {
     return (
         <Link to={`/p/${id}`} className='project-item-wrapper'>
-            <div className="image-container">
-                <img src={require('../../assets/images/space.png')} alt='' />
-            </div>
+            <div className="image-container" style={{
+              backgroundImage: `url(${background})`,
+              backgroundPosition: "center",
+              backgroundSize: "cover",
+              backgroundRepeat: "no-repeat",
+            }} />
             <div className="title-container">
                 {data.title}
             </div>
