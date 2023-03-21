f8949bcdfa059cacb80ce60a3b3cc8fc1c75a00c
Uthaeus <romanlavery@gmail.com>
2023-03-21 10:51:21 -0600

    built contact page structure and added styling

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,9 +1,34 @@
-
-
 function ContactPage() {
-    return (
-        <h1>Contact Page</h1>
-    );
+  return (
+    <div className="contact-container">
+      <div className="contact-image-wrapper">
+        <img
+          className="contact-image"
+          src={require("../assets/images/space.png")}
+          alt=""
+        />
+      </div>
+
+      <div className="contact-info-wrapper">
+        <h2>Homer J. Simpson</h2>
+        <p>123 Anystreet, Springfield, IL</p>
+        <p>(555)555-5555</p>
+        <p>homerjsimpson@fakemail.net</p>
+
+        <div className="contact-social-wrapper">
+          <a href="www.instagram.com" target="_blank">
+            Insta
+          </a>
+          <a href="www.facebook.com" target="_blank">
+            Faceb
+          </a>
+          <a href="www.tiktok.com" target="_blank">
+            TikTok
+          </a>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default ContactPage;
