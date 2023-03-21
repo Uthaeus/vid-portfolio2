6e35439e0e55f6ef34bfbebf82de47e63c48690a
Uthaeus <romanlavery@gmail.com>
2023-03-20 21:01:44 -0600

    created modal component using createPortal, added styling

--- src/pages/ProjectDetail.js
+++ src/pages/ProjectDetail.js
@@ -1,33 +1,60 @@
+import { useState } from "react";
 import { useParams } from "react-router-dom";
 
 import { DUMMY_DATA } from "../store/dummy_data";
 import background from "../assets/images/space.png";
+import Modal from "../UI/Modal";
 
 function ProjectDetail() {
   let { projectId } = useParams();
+  const [modalIsOpen, setModalIsOpen] = useState(false);
 
   let project = DUMMY_DATA.find((item) => projectId === item.id);
 
   let { title, description, thumbnail_url, video_url } = project.data;
 
-  return (
-    <div className="project-detail-wrapper">
-      <div className="left-column-wrapper">
-        <div className="title-detail">{title}</div>
-        <p className="description-detail">{description}</p>
+  function openModalHandler() {
+    setModalIsOpen(true);
+  }
+
+  function closeModalHandler() {
+    setModalIsOpen(false);
+  }
+
+  let modalContent = (
+    <div className="modal-content-wrapper">
+      <div className="modal-image-wrapper">
+        <img src={require("../assets/images/space.png")} alt="" />
       </div>
-      <div className="right-column-wrapper">
-        <div
-          className="thumbnail-detail"
-          style={{
-            backgroundImage: `url(${background})`,
-            backgroundPosition: "center",
-            backgroundSize: "cover",
-            backgroundRepeat: "no-repeat",
-          }}
-        />
+      <div className="modal-button-wrapper">
+        <button className="modal-button" onClick={closeModalHandler}>Close</button>
       </div>
     </div>
+  );
+
+  return (
+    <>
+      <div className="project-detail-wrapper">
+        <div className="left-column-wrapper">
+          <div className="title-detail">{title}</div>
+          <p className="description-detail">{description}</p>
+        </div>
+
+        <div className="right-column-wrapper">
+          <div
+            className="thumbnail-detail"
+            onClick={openModalHandler}
+            style={{
+              backgroundImage: `url(${background})`,
+              backgroundPosition: "center",
+              backgroundSize: "cover",
+              backgroundRepeat: "no-repeat",
+            }}
+          />
+        </div>
+      </div>
+      {modalIsOpen && <Modal onClose={closeModalHandler}>{modalContent}</Modal>}
+    </>
   );
 }
 
