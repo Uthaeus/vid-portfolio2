905014022e43fcb184a7e745caae3b096cfd47b2
Uthaeus <romanlavery@gmail.com>
2023-03-24 13:59:38 -0600

    refactored new project component, moved form to newProjectForm component, moved styling to seperate file

---
+++ src/components/projects/NewProjectForm.js
@@ -0,0 +1,83 @@
+import { useState } from "react";
+
+const initialData = {
+  title: "",
+  category: "",
+  videoUrl: "",
+  thumbnailUrl: "",
+  description: "",
+};
+
+function NewProjectForm() {
+  const [formData, setFormData] = useState(initialData);
+
+  function clearForm() {
+    setFormData(initialData);
+  }
+
+  function changeHandler(event) {
+    setFormData({
+      ...formData,
+      [event.target.name]: event.target.value,
+    });
+  }
+
+  function submitHandler(event) {
+    event.preventDefault();
+
+    console.log("submit handler new project", formData);
+    clearForm();
+  }
+
+  return (
+    <form onSubmit={submitHandler}>
+      <div className="group-two">
+        <input
+          type="text"
+          name="title"
+          placeholder="Project Title"
+          value={formData.title}
+          onChange={changeHandler}
+        />
+        <input
+          type="text"
+          name="category"
+          placeholder="Project Category"
+          value={formData.category}
+          onChange={changeHandler}
+        />
+      </div>
+      <div className="group-two">
+        <input
+          type="url"
+          name="videoUrl"
+          placeholder="Video URL"
+          value={formData.videoUrl}
+          onChange={changeHandler}
+        />
+        <input
+          type="url"
+          name="thumbnailUrl"
+          placeholder="Thumbnail URL"
+          value={formData.thumbnailUrl}
+          onChange={changeHandler}
+        />
+      </div>
+      <div className="group-one">
+        <textarea
+          name="description"
+          placeholder="Description of project..."
+          rows={8}
+          cols={30}
+          value={formData.description}
+          onChange={changeHandler}
+        />
+      </div>
+      <div className="form-actions">
+        <button type="submit">Save</button>
+      </div>
+    </form>
+  );
+}
+
+export default NewProjectForm;
--- src/pages/NewProject.js
+++ src/pages/NewProject.js
@@ -1,84 +1,14 @@
-import { useState } from "react";
 
-const initialData = {
-  title: "",
-  category: "",
-  videoUrl: "",
-  thumbnailUrl: "",
-  description: "",
-};
+
+import NewProjectForm from "../components/projects/NewProjectForm";
 
 function NewProject() {
-  const [formData, setFormData] = useState(initialData);
-
-  function clearForm() {
-    setFormData(initialData);
-  }
-
-  function changeHandler(event) {
-    setFormData({
-      ...formData,
-      [event.target.name]: event.target.value,
-    });
-  }
-
-  function submitHandler(event) {
-    event.preventDefault();
-
-    console.log("submit handler new project", formData);
-    clearForm();
-  }
+  
 
   return (
     <div className="new-project-container">
       <h1>Add Project</h1>
-      <form onSubmit={submitHandler}>
-        <div className="group-two">
-          <input
-            type="text"
-            name="title"
-            placeholder="Project Title"
-            value={formData.title}
-            onChange={changeHandler}
-          />
-          <input
-            type="text"
-            name="category"
-            placeholder="Project Category"
-            value={formData.category}
-            onChange={changeHandler}
-          />
-        </div>
-        <div className="group-two">
-          <input
-            type="text"
-            name="videoUrl"
-            placeholder="Video URL"
-            value={formData.videoUrl}
-            onChange={changeHandler}
-          />
-          <input
-            type="text"
-            name="thumbnailUrl"
-            placeholder="Thumbnail URL"
-            value={formData.thumbnailUrl}
-            onChange={changeHandler}
-          />
-        </div>
-        <div className="group-one">
-          <textarea
-            name="description"
-            placeholder="Description of project..."
-            rows={8}
-            cols={30}
-            value={formData.description}
-            onChange={changeHandler}
-          />
-        </div>
-        <div className="form-actions">
-          <button type="submit">Save</button>
-        </div>
-      </form>
+      <NewProjectForm />
     </div>
   );
 }
